4e038923c1cd43b47e7cf218014d3d28b2b322c9
Rafaela <rafaela.imiani@outlook.com>
2025-09-11 12:09:11 -0300

    introdução ao java

---
+++ helloworld/src/helloworld/Variaveis.java
@@ -0,0 +1,23 @@
+package helloworld;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+       int peso = 50;
+       
+       char opcao = 'S';
+       
+       float valor = 100;       
+       
+       System.out.println("o valor da variavel peso é: " + peso);
+       
+       System.out.println("o valor da variavel opcao é: " + opcao);
+       
+       System.out.printf("o valor da variavel valor é: %.2f \n" + valor);
+       
+       
+	}
+
+}
+
